9ec262da2746baf29161c73ff21d0b6c297dd790
Tadej23 <47983987+Tadej23@users.noreply.github.com>
2022-03-14 12:10:44 +0100

    First commit

--- AOC_1/Program.cs
+++ AOC_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AOC_1
 {
@@ -6,7 +7,21 @@ namespace AOC_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!");
+        //How many measurements are larger than the previous measurement?
+        //Input
+        string[] lines = File.ReadAllLines("D:\\FIŠ\\AOC\\AOC_1\\AOC_1.txt");
+        int[] linesInt = Array.ConvertAll(lines, int.Parse);
+
+        int numOfincreased = 0;
+
+        for (int i=1; i<linesInt.Length; i++)
+            {
+                if (linesInt[i] > linesInt[i - 1]){ numOfincreased++;  }  
+            }
+            Console.WriteLine("Rezultat:" + numOfincreased.ToString()); 
+
+
         }
     }
 }
