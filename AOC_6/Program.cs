7bd87bafde943c0b00fdefa8e92f174f064dbed7
Tadej23 <47983987+Tadej23@users.noreply.github.com>
2022-03-14 15:20:18 +0100

    6.2 dictionary

--- AOC_6/Program.cs
+++ AOC_6/Program.cs
@@ -24,7 +24,7 @@ namespace AOC_6
                 1, 1, 5, 5, 2, 1, 1, 5, 2, 4, 5, 5, 1, 1, 5, 1, 5, 5, 1, 1, 3, 3, 1, 1, 3, 1 };
             
 
-            for (int i = 0; i < 256; i++)
+            for (int i = 0; i < 80; i++)
             {
                 int addNew = 0;
                 for( var j=0; j<initialState.Count; j++)
@@ -46,9 +46,67 @@ namespace AOC_6
 
             }
 
-            Console.WriteLine("Rezultat: " + initialState.Count.ToString());
+            Console.WriteLine("Rezultat 6.1: " + initialState.Count.ToString());
+
+            //-------6.2--------
+            /*
+            List<int> initialState2 = new List<int>() {2, 1, 1, 4, 4, 1, 3, 4, 2, 4, 2, 1, 1, 4, 3, 5, 1, 1, 5, 1, 1, 5, 4, 5, 4,
+                1, 5, 1, 3, 1, 4, 2, 3, 2, 1, 2, 5, 5, 2, 3, 1, 2, 3, 3, 1, 4, 3, 1, 1, 1, 1, 5, 2, 1, 1,
+                1, 5, 3, 3, 2, 1, 4, 1, 1, 1, 3, 1, 1, 5, 5, 1, 4, 4, 4, 4, 5, 1, 5, 1, 1, 5, 5, 2, 2, 5,
+                4, 1, 5, 4, 1, 4, 1, 1, 1, 1, 5, 3, 2, 4, 1, 1, 1, 4, 4, 1, 2, 1, 1, 5, 2, 1, 1, 1, 4, 4,
+                4, 4, 3, 3, 1, 1, 5, 1, 5, 2, 1, 4, 1, 2, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 2, 1, 5, 5, 2, 1, 1,
+                1, 4, 4, 1, 4, 2, 3, 3, 3, 3, 3, 5, 4, 1, 5, 1, 4, 5, 5, 1, 1, 1, 4, 1, 2, 4, 4, 1, 2, 3, 3, 3,
+                3, 5, 1, 4, 2, 5, 5, 2, 1, 1, 1, 1, 3, 3, 1, 1, 2, 3, 2, 5, 4, 2, 1, 1, 2, 2, 2, 1, 3, 1, 5, 4,
+                1, 1, 5, 3, 3, 2, 2, 3, 1, 1, 1, 1, 2, 4, 2, 2, 5, 1, 2, 4, 2, 1, 1, 3, 2, 5, 5, 3, 1, 3, 3, 1,
+                4, 1, 1, 5, 5, 1, 5, 4, 1, 1, 1, 1, 2, 3, 3, 1, 2, 3, 1, 5, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 5,
+                1, 1, 5, 5, 2, 1, 1, 5, 2, 4, 5, 5, 1, 1, 5, 1, 5, 5, 1, 1, 3, 3, 1, 1, 3, 1 };
+            */
+
+            List<int> initialState2 = new List<int>() { 0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 0, 1, 2, 2, 2, 3, 3, 4, 4, 5, 7, 8 };
+
+            var FishSchool = new Dictionary<int, double>() 
+            { 
+                { 0, 0 }, 
+                { 6, 0 },
+                { 7, 0 },
+                { 8, 0 },
+            };
+
+            foreach (int fish in initialState2)
+            {               
+                if (FishSchool.ContainsKey(fish)) { FishSchool[fish] += 1; }
+                else { FishSchool.Add(fish, 1); }
+            }
 
 
+            for (int i = 0; i < 256; i++)
+            {
+                double newFish= FishSchool[0];
+                FishSchool[0] = FishSchool[1];
+                FishSchool[1] = FishSchool[2];
+                FishSchool[2] = FishSchool[3];
+                FishSchool[3] = FishSchool[4];
+                FishSchool[4] = FishSchool[5];
+                FishSchool[5] = FishSchool[6] + newFish;
+                FishSchool[6] = FishSchool[7];
+                FishSchool[7] = FishSchool[8];
+                FishSchool[8] = newFish;
+            }
+
+
+            double highSchool = 0;
+
+            foreach (KeyValuePair<int,double> kvp in FishSchool)
+            {
+                highSchool += kvp.Value;
+            }
+
+
+            Console.WriteLine("Rezultat 6.2: " + highSchool.ToString());
+
+
+
+            Console.ReadKey();
 
         }
     }
