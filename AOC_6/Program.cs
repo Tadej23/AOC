d0139c6d4ff120a3b9a74d4280dd8517815fe63a
Tadej23 <47983987+Tadej23@users.noreply.github.com>
2022-03-14 13:13:30 +0100

    AOC 6.1 ok, AOC 6.2 ni ok

--- AOC_6/Program.cs
+++ AOC_6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AOC_6
 {
@@ -6,7 +7,49 @@ namespace AOC_6
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
+            //How many lanternfish would there be after 80 days?
+
+            // Input
+            List<int> initialState = new List<int>() {2, 1, 1, 4, 4, 1, 3, 4, 2, 4, 2, 1, 1, 4, 3, 5, 1, 1, 5, 1, 1, 5, 4, 5, 4,
+                1, 5, 1, 3, 1, 4, 2, 3, 2, 1, 2, 5, 5, 2, 3, 1, 2, 3, 3, 1, 4, 3, 1, 1, 1, 1, 5, 2, 1, 1,
+                1, 5, 3, 3, 2, 1, 4, 1, 1, 1, 3, 1, 1, 5, 5, 1, 4, 4, 4, 4, 5, 1, 5, 1, 1, 5, 5, 2, 2, 5,
+                4, 1, 5, 4, 1, 4, 1, 1, 1, 1, 5, 3, 2, 4, 1, 1, 1, 4, 4, 1, 2, 1, 1, 5, 2, 1, 1, 1, 4, 4,
+                4, 4, 3, 3, 1, 1, 5, 1, 5, 2, 1, 4, 1, 2, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 2, 1, 5, 5, 2, 1, 1,
+                1, 4, 4, 1, 4, 2, 3, 3, 3, 3, 3, 5, 4, 1, 5, 1, 4, 5, 5, 1, 1, 1, 4, 1, 2, 4, 4, 1, 2, 3, 3, 3,
+                3, 5, 1, 4, 2, 5, 5, 2, 1, 1, 1, 1, 3, 3, 1, 1, 2, 3, 2, 5, 4, 2, 1, 1, 2, 2, 2, 1, 3, 1, 5, 4,
+                1, 1, 5, 3, 3, 2, 2, 3, 1, 1, 1, 1, 2, 4, 2, 2, 5, 1, 2, 4, 2, 1, 1, 3, 2, 5, 5, 3, 1, 3, 3, 1,
+                4, 1, 1, 5, 5, 1, 5, 4, 1, 1, 1, 1, 2, 3, 3, 1, 2, 3, 1, 5, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 5,
+                1, 1, 5, 5, 2, 1, 1, 5, 2, 4, 5, 5, 1, 1, 5, 1, 5, 5, 1, 1, 3, 3, 1, 1, 3, 1 };
+            
+
+            for (int i = 0; i < 256; i++)
+            {
+                int addNew = 0;
+                for( var j=0; j<initialState.Count; j++)
+                {
+                    initialState[j] = initialState[j] - 1;
+                    if (initialState[j]<0) 
+                    { 
+                        initialState[j]= 6;
+                        addNew++;
+                    
+                    }
+
+                }
+
+                for(int h=0; h<addNew; h++)
+                {
+                    initialState.Add(8);
+                }
+
+            }
+
+            Console.WriteLine("Rezultat: " + initialState.Count.ToString());
+
+
+
         }
     }
 }
